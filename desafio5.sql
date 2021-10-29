e3d20c1b12068450fde4866ee3ccc0045f97118e
Murilo Ribeiro <murilorsv14@gmail.com>
2021-10-29 16:11:59 -0300

    desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,11 @@
+CREATE VIEW top_2_hits_do_momento AS 
+SELECT
+m.Nome AS 'cancao',
+COUNT(*) AS 'reproducoes'
+FROM SpotifyClone.Musica AS m
+LEFT JOIN SpotifyClone.Historico_musica as hm
+ON m.Musica_ID = hm.Musica_ID
+LEFT JOIN SpotifyClone.Usuario as u
+ON u.Usuario_ID = hm.Usuario_ID
+GROUP BY m.Nome
+ORDER BY reproducoes DESC, cancao;
