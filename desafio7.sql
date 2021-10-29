53cbe8cb4c34cbc6dee3e348dd7a8285bb553fd7
Murilo Ribeiro <murilorsv14@gmail.com>
2021-10-29 17:12:01 -0300

    desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,15 @@
+CREATE VIEW perfil_artistas
+AS
+SELECT
+ ar.Nome AS 'artista',
+ ab.Nome AS 'album',
+ COUNT(*) AS 'seguidores'
+-- ab.Artista_ID
+-- COUNT(Usuario_ID) AS 'seguidores'
+FROM SpotifyClone.Artista AS ar
+LEFT JOIN SpotifyClone.Album AS ab
+ON ar.Artista_ID = ab.Artista_ID
+LEFT JOIN SpotifyClone.Seguindo_artistas as sa
+ON sa.Artista_ID = ar.Artista_ID
+GROUP BY ab.Album_ID, ar.Artista_ID
+ORDER BY seguidores DESC, artista;
